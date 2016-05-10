7165e2bdf6bbf8391ef0f14dcc3022e340b9bfb4
marceca <marceca@gmail.com>
2016-05-09 20:10:35 -0400

    Add files via upload

--- php/insert.php
+++ php/insert.php
@@ -15,10 +15,11 @@ if (mysqli_connect_errno()) {
 }
 
 if($firstName && $lastName && $userName && $password && $secondPassword && $age && $email){
-	
+	if($password != $secondPassword){
+		die("Your passwords are not the same!");
+	}
 	mysqli_query($db, "INSERT INTO users(firstName, lastName, username, password, age, email) VALUES('$firstName','$lastName','$userName','$password','$age','$email')");
-	
-	
+		
 } else {
 	
 	echo "You did not fill out the entire form!";
@@ -28,5 +29,32 @@ if($firstName && $lastName && $userName && $password && $secondPassword && $age 
 
 mysqli_close($db);
 
-
 ?>
+
+<html>
+	<head>
+		<meta charset = "UTF-8">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+		<title>Welcome to Sunday Salts</title>
+		<link type = "text/css" rel = "stylesheet" href = "../css/sundaySalts.css"></link>
+	</head>
+	<body>
+		<div id="header"><h2 id="fontSize">Sunday Salts</h2></div>
+			<?php include 'insertNav.php';?>
+		<div id="mid">
+			<div id="mainImgMove">
+				<img src="../images/allSundaySalts" id="mainImgResize" />
+			</div>
+			<p class="center">This is center stage</p>
+		
+		
+		</div>
+
+			<div>
+				<a href="register.php"><h4>Register</h4></a>
+			</div>
+		</div>
+		<div class="dots" id="footer">&copy;Sunday Salts</div>
+		<script src="jquery/highlight.js"></script>
+	</body>
+</html>
---
+++ php/nav.php
@@ -0,0 +1,11 @@
+<center>
+	<h3>
+		<nav id="navigation">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="flavors.php">Flavors</a></li>
+				<li><a href="contact.php">Contact us</a></li>
+			</ul>
+		</nav>
+	</h3>
+</center>
